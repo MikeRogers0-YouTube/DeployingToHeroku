ba1bdd6dd509c2516efd998f880a819e2161d594
Mike Rogers <me@mikerogers.io>
2020-11-03 19:39:25 +0000

    Adding Sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: redirect('/blogs')
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  mount Sidekiq::Web => '/sidekiq'
 end
