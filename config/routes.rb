a13426b4a019b02ef4b27624542e7602b8c971d7
Mike Rogers <me@mikerogers.io>
2020-11-03 19:55:55 +0000

    Lets assume if sidekiq::web is preset, we want that route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: redirect('/blogs')
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  if defined?(Sidekiq) && ENV['REDIS_URL'].present?
+  if defined?(Sidekiq) && defined?(Sidekiq::Web)
     mount Sidekiq::Web => '/sidekiq'
   end
 end
