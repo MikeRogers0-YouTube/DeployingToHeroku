79cef98015175508791527629107a4d124a51b45
Mike Rogers <me@mikerogers.io>
2020-11-03 19:41:13 +0000

    Requiring sidekiq in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: redirect('/blogs')
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  mount Sidekiq::Web => '/sidekiq'
+  if defined?(Sidekiq) && defined?(Sidekiq::Cron)
+    require 'sidekiq/web'
+    require 'sidekiq/cron/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
